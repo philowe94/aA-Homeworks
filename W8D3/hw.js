d260ccd0a2235a5a9f3bbe0ed18b9768812bd8f3
Philip Lowe <philip.lowe94@gmail.com>
2021-04-13 20:04:50 -0400

    madlibs

--- W8D3/hw.js
+++ W8D3/hw.js
@@ -1,45 +1,23 @@
-function mysteryScoping1() {
+function mysteryScoping4() {
     var x = 'out of block';
     if (true) {
       var x = 'in block';  
       console.log(x);
     }
+    var x = 'out of block again';
     console.log(x);
   }
-  
-  function mysteryScoping2() {
-    const x = 'out of block';
-    if (true) {
-      const x = 'in block';  
-      console.log(x);
-    }
-    console.log(x);
-  }
-  
-  function mysteryScoping3() {
-    const x = 'out of block';
-    if (true) {
-      var x = 'in block';  
-      console.log(x);
-    }
-    console.log(x);
-  }
-  
-  function mysteryScoping4() {
-    let x = 'out of block';
-    if (true) {
-      let x = 'in block';  
-      console.log(x);
-    }
-    console.log(x);
-  }
-  
-  function mysteryScoping5() {
-    let x = 'out of block';
-    if (true) {
-      let x = 'in block';  
-      console.log(x);
-    }
-    let x = 'out of block again';
-    console.log(x);
-  }
+
+mysteryScoping4()
+
+//var var was in block in block
+//const const was in block out of block
+//const var errored, because var can't be declared if there is a const of the same name in scope
+//let let was in block out of block
+// let let let was error because the outer let cant overwrite the earlier let (but var var var works just fine)
+
+function madlibs(word1, word2, word3) {
+    console.log(`We shall ${word1.toUpperCase()} the ${word2.toUpperCase()} ${word3.toUpperCase()}`);
+}
+
+madlibs('make', 'best', 'guac')
